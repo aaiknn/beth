0c1b75c0b8ec0aed1321b7295740b89b53715c07
aaiknn <aaiknn.git@gmail.com>
2022-02-14 12:34:56 -0100

    Engaging modularisation

--- beth.py
+++ beth.py
@@ -5,19 +5,19 @@ from src.dns import collect
 from src.domainscan import scan
 from src.reverseip import reverse
 
+from src.globals import structures as st 
+
 jobs = args.parse()
 
-glorious_separation = '------------------------------------------------'
-
 def end_graciously():
-    print(glorious_separation)
+    print(st['glorious_separation'])
 
 for module in jobs:
   try:
     for target in jobs[module]:
-      print(glorious_separation)
+      print(st['glorious_separation'])
       print('module:', module, '\ntarget:', target)
-      print(glorious_separation)
+      print(st['glorious_separation'])
 
       if module == 'dns':
         records = collect(target)
